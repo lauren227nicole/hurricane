3a0fe25a8016cff38a0d4c55a262477b5b2b16ed
rbrown530 <r_yan@outlook.com>
2015-10-08 10:35:01 -0600

    10/06/15 - Initial commit to repository

---
+++ src/Player.java
@@ -0,0 +1,39 @@
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ryan
+ */
+public class Player implements Serializable {
+
+    
+    private String name;
+    private int money;
+    
+    public Player() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+    
+}
