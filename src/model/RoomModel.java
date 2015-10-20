d93171a0044f16d0734e8e674f68caa1f56fb389
Sean <Sean@192.168.2.9>
2015-10-20 13:20:21 -0400

    10/6/15 - Lauren first commit attempt

---
+++ src/model/RoomModel.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Sean
+ */
+public class RoomModel implements Serializable {
+    InventoryItem inventory;
+
+    public RoomModel() {
+    
+    }
+        
+    public InventoryItem getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(InventoryItem inventory) {
+        this.inventory = inventory;
+    }
+    
+}
