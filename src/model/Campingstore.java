ba5c479b9ddfb61c7ee71673a759720f295f7e1b
rbrown530 <r_yan@outlook.com>
2015-10-24 23:17:27 -0600

    10/06/15 - Initial commit to repository

--- src/model/Campingstore.java
+++ src/model/Campingstore.java
@@ -5,26 +5,74 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Ryan
  */
-public class Campingstore {
+public class Campingstore implements Serializable{
     
     
-    String name;
-    
-    
-    public static void main(String[] args){
-    
-    Campingstore aisle1 = new Campingstore();
-    
-    aisle1.name = "tents";
-    aisle1.name = "lighting";
-    aisle1.name = "cooking";
-    aisle1.name = "gamemenu";
-    
+    private String lightingaisle;
+    private String cookingaisle;
+    private String tentaisle;
+
+    public String getLightingaisle() {
+        return lightingaisle;
     }
+
+    public void setLightingaisle(String lightingaisle) {
+        this.lightingaisle = lightingaisle;
+    }
+
+    public String getCookingaisle() {
+        return cookingaisle;
+    }
+
+    public void setCookingaisle(String cookingaisle) {
+        this.cookingaisle = cookingaisle;
+    }
+
+    public String getTentaisle() {
+        return tentaisle;
+    }
+
+    public void setTentaisle(String tentaisle) {
+        this.tentaisle = tentaisle;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.lightingaisle);
+        hash = 37 * hash + Objects.hashCode(this.cookingaisle);
+        hash = 37 * hash + Objects.hashCode(this.tentaisle);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Campingstore other = (Campingstore) obj;
+        if (!Objects.equals(this.lightingaisle, other.lightingaisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.cookingaisle, other.cookingaisle)) {
+            return false;
+        }
+        if (!Objects.equals(this.tentaisle, other.tentaisle)) {
+            return false;
+        }
+        return true;
+    }
+    
     
     
 }
